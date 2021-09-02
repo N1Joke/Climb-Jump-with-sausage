5eb9e98a6720dc84d36d2cb25c2c9863858caf5c
Vladislav Ivanov <JokeIvanov@yandex.ru>
2021-09-02 19:45:39 +0300

    first commit

--- Assets/Scripts/LevelBuilder.cs
+++ Assets/Scripts/LevelBuilder.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class LevelBuilder : MonoBehaviour
@@ -10,6 +8,9 @@ public class LevelBuilder : MonoBehaviour
     [Range(6, 25)]
     [SerializeField] private int _amountOfPlatforms = 6;
     [SerializeField] private int _DistanceBetweenLevels = 4;
+    [SerializeField] private GameObject _endLevelCheckBox;
+    [SerializeField] private float _CheckBoxOffsetY = 1;
+    [SerializeField] private GameObject _deathTrigger;
     [Header("Platform settrings")]
     [SerializeField] private int _minXLenthPlatform = 1;
     [SerializeField] private int _maxXLenthPlatform = 6;
@@ -17,8 +18,15 @@ public class LevelBuilder : MonoBehaviour
     [SerializeField] private int _minYOffsetPlatform = 1;
     [SerializeField] private int _maxYOffsetPlatform = 5;
 
+    private ChankEndChecker _chankEndChecker;
+    private float _firstOffsetX = 0;
 
     private void Start()
+    {
+        BuildChanck();
+    }
+
+    private void BuildChanck()
     {
         for (int i = 0; i < _amountOfPlatforms; i++)
         {
@@ -29,9 +37,26 @@ public class LevelBuilder : MonoBehaviour
 
             platform.transform.localScale = new Vector3(lengthPlatformX, platform.transform.localScale.y, platform.transform.localScale.z);
 
-            platform.transform.position = new Vector3(_currentPlatform.position.x + _currentPlatform.localScale.x / 2 + lengthPlatformX / 2, _currentPlatform.position.y + offsetPlatformY, platform.transform.position.z);
-
+            platform.transform.position = new Vector3(_currentPlatform.position.x + _currentPlatform.localScale.x / 2 + lengthPlatformX / 2 + _firstOffsetX, _currentPlatform.position.y + offsetPlatformY, platform.transform.position.z);
+            _firstOffsetX = 0;
             _currentPlatform = platform.transform;
+
+            if (i == _amountOfPlatforms - 1)
+            {
+                GameObject checkBox = Instantiate(_endLevelCheckBox, transform);
+                checkBox.transform.position = new Vector3(_currentPlatform.position.x, _currentPlatform.position.y + _currentPlatform.localScale.y / 2 + _CheckBoxOffsetY, _currentPlatform.position.z);
+                _chankEndChecker = checkBox.GetComponent<ChankEndChecker>();
+                _chankEndChecker.OnEndOfChanckDelegare += EndLevel;
+
+                GameObject deathTrigger = Instantiate(_deathTrigger, transform);
+                deathTrigger.transform.position = new Vector3(transform.position.x + _DistanceBetweenLevels / 2, transform.position.y, transform.transform.position.z);
+            }
         }
+    }
+
+    private void EndLevel()
+    {
+        _firstOffsetX = _DistanceBetweenLevels;
+        BuildChanck();
     }
 }
--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,23 +6,109 @@ using UnityEngine;
 public class Mover : MonoBehaviour
 {
     [SerializeField] private Camera _camera;
-    [SerializeField] private Rigidbody _rigidbody;
+    [SerializeField] private float _xCoefficient = 80;
+    [SerializeField] private float _yCoefficient = 80;
 
-    private Vector3 _tapping;
-    private Vector3 _release;
-    
+    private Rigidbody _rigidbody;
+    private Vector2 _tapping;
+    private Vector2 _release;
+    private bool _grounded;
+
+    private void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
     private void Update()
     {
-        if (Input.touchCount > 0)
+        if (Input.GetMouseButtonDown(0))
         {
-            Touch touch = Input.GetTouch(0);
+            _tapping = Input.mousePosition;
 
-            // Update the Text on the screen depending on current position of the touch each frame
-            Debug.Log("Touch Position : " + touch.position);
+            Debug.Log("Touch Position down: " + _tapping);
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            _release = Input.mousePosition;
+
+            Debug.Log("Touch Position up: " + _release);
+
+            if (_grounded)
+                PushObject();
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            DrawGizmos();
+        }
+    }
+
+    private void PushObject()
+    {
+        Vector3 point = transform.position;
+        Vector3 start = point;
+        Debug.Log("start point " + point);
+        if (_tapping.y < _release.y)
+        {
+            point.y = (_release.y - _tapping.y) / _yCoefficient;
         }
         else
         {
-            Debug.Log("No touch contacts");
+            point.y = (_tapping.y - _release.y) / _yCoefficient * -1;
         }
+
+        if (_tapping.x < _release.x)
+        {
+            point.x = (_release.x - _tapping.x) / _xCoefficient;
+        }
+        else
+        {
+            point.x = (_tapping.x - _release.x) / _xCoefficient * -1;
+        }
+
+        Vector3 direction = new Vector3(point.x * -1, point.y * -1, 0);
+        Debug.Log("new point " + direction);
+        Debug.DrawRay(start, direction, Color.yellow, 30f);
+        _rigidbody.AddForce(direction, ForceMode.Impulse);
+    }
+
+    private void DrawGizmos()
+    {
+        Vector3 point = transform.position;
+        Vector3 start = point;
+
+        Vector3 vector3rel = Input.mousePosition;
+
+        if (_tapping.y < vector3rel.y)
+        {
+            point.y = (vector3rel.y - _tapping.y) / _yCoefficient;
+        }
+        else
+        {
+            point.y = (_tapping.y - vector3rel.y) / _yCoefficient * -1;
+        }
+
+        if (_tapping.x < vector3rel.x)
+        {
+            point.x = (vector3rel.x - _tapping.x) / _xCoefficient;
+        }
+        else
+        {
+            point.x = (_tapping.x - vector3rel.x) / _xCoefficient * -1;
+        }
+
+        Vector3 direction = new Vector3(point.x * -1, point.y * -1, 0);
+        Debug.DrawRay(start, direction, Color.yellow);
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        _grounded = true;
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        _grounded = false;
     }
 }
